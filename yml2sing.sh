6a05ae6cb2b8bf9dc3f1c1450a96986c979ff725
Olivier Kirsh <olivier.kirsh@univ-paris-diderot.fr>
2020-07-27 15:30:50 +0200

    maj update

--- yml2sing.sh
+++ yml2sing.sh
@@ -3,23 +3,30 @@
 # This script execute a singularity build command to create a sing image from a yml file
 # 
 # Usage : create a folder containing a yml file and type un a terminal $ yml2sing.sh <image-name> 
-# read my documentation about singularity https://github.com/kirsho/Singularity/blob/master/Intro2Singularity.md
+# It will create a singularity image named envname.simg with the tools installed in an conda env named with the basename of the yml.
+# read my documentation about singularity 
+# https://github.com/kirsho/Singularity/blob/master/Intro2Singularity.md
+# https://github.com/kirsho/yml2sing/blob/master/yml2sing.sh
 # contact : Olivier Kirsh <olivier.kirsh@u-paris.fr>
-# date : 20200728
+# date : 20200727
 
 # download my definition file for singularity
-# For easy push in shubn the definition file is named Singularity
+# For easy push in shub the definition file is named Singularity
 #wget https://raw.githubusercontent.com/kirsho/Singularity/master/Singularity
 
+# folder cleanup
 rm Singularity*  && \
 
+# Download definition file
 wget https://raw.githubusercontent.com/kirsho/yml2sing/master/Singularity  && \
 
-# extract yml file name from work directory
+# extract yml file name (rename it to change 
 defile="$(ls *.y*ml)"  && \
 
 # mofify the definition file (Singularity)
 sed -i "s/env-name.yml/${defile}/g" Singularity  && \
+
+sed -i "s/envname/${defile%%.y*ml}/g" Singularity  && \
 
 # Build the image
 sudo singularity build $1.simg Singularity
